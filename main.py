de094c3eab4ecb0274c3e307f84f634766cb23bd
sergiyzin <sergiy.zinkivsky@gmail.com>
2026-02-08 23:37:46 +0200

    Left only DZ #1

--- main.py
+++
@@ -1,7 +0,0 @@
-import re
-
-text = "Python - потужна; проста, універсальна: мова!"
-pattern = r"[;,\-:!\s]+"
-elements = re.split(pattern, text)
-
-print(elements)  # Виведе список частин, розділених пунктуацією
